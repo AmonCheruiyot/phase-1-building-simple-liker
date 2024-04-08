c8e908b2df44b52d7201cd9c3d127ef2f39d8052
AmonCheruiyot <161847144+AmonCheruiyot@users.noreply.github.com>
2024-04-08 20:02:55 +0300

    Initial commit

--- main.js
+++ main.js
@@ -1,8 +1,48 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
-// Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', function() {
+  const errorModal = document.getElementById('modal');
+  errorModal.classList.add('hidden');
+
+  document.addEventListener('click', function(e) {
+    if (e.target.classList.contains('like-glyph')) {
+      likeOrUnlike(e.target);
+    }
+  });
+});
+
+function likeOrUnlike(heart) {
+  if (heart.innerText == EMPTY_HEART) {
+    mimicServerCall()
+      .then(() => {
+        heart.innerText = FULL_HEART;
+        heart.classList.add('activated-heart');
+      })
+      .catch((error) => {
+        displayError(error);
+      });
+  } else {
+    mimicServerCall()
+      .then(() => {
+        heart.innerText = EMPTY_HEART;
+        heart.classList.remove('activated-heart');
+      })
+      .catch((error) => {
+        displayError(error);
+      });
+  }
+}
+
+function displayError(error) {
+  const errorModal = document.getElementById('modal');
+  const errorMessage = document.getElementById('modal-message');
+  errorMessage.innerText = error;
+  errorModal.classList.remove('hidden');
+  setTimeout(() => {
+    errorModal.classList.add('hidden');
+  }, 3000);
+}
 
 
 
@@ -10,7 +50,6 @@ const FULL_HEART = '♥'
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
-
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
